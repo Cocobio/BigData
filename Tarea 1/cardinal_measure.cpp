5209b2e244ef2d96669820df5b554e4a0bfcb91c
Cocobio <iosorio@udec.cl>
2020-11-12 14:12:57 -0300

    Cardinal print option added.

--- Tarea 1/cardinal_measure.cpp
+++ Tarea 1/cardinal_measure.cpp
@@ -7,25 +7,26 @@
 
 using namespace std;
 
-void processDNA(string file, int k);
+void processDNA(string file, int k, bool print);
 
 int main(int argn, char **argsv) {
-	if (argn!=3) {
-		cout << "Incorrect input. Expected: fastq_file_path k" << endl;
+	if (argn!=4) {
+		cout << "Incorrect input. Expected: fastq_file_path k print=1" << endl;
 		return -1;
 	}
 
 	// Filter basic options
 	string file_path(argsv[1]);
 	int k = atoi(argsv[2]);		// for k-mers
+	bool p = atoi(argsv[3]);
 
-	processDNA(file_path, k);
+	processDNA(file_path, k, p);
 
 	return 0;
 }
 
 
-void processDNA(string file, int k) {
+void processDNA(string file, int k, bool print) {
 	unordered_set<string> cardinal;
 	string tmp;
 
@@ -49,6 +50,11 @@ void processDNA(string file, int k) {
 	cout << "Cardinal de " << file << " con kmers de " << k << ": " << cardinal.size() << endl;
 
 	cout << "Time taken: " << ((double)clock() - start)/CLOCKS_PER_SEC << " [s]" << endl;
+
+	if (!print)
+		return;
+	for(auto &kmer : cardinal)
+		cout << kmer << endl;
 }
 
 
